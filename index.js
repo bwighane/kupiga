5a97094dc33f47480c3e0bb91066eca4de305006
ibwighane <bsmwalwanda@gmail.com>
2019-01-08 17:45:46 +0200

    intial commit

---
+++ index.js
@@ -0,0 +1,65 @@
+const URL = require('url')
+
+const getClientIp = (req) => {
+  if (req && req.origin) {
+    return req.origin
+  }
+  return ''
+}
+
+exports.buildOrchestration = (name, beforeTimestamp, method, url, requestHeaders, requestContent, res, body, req = null) => {
+  // response headers
+
+  const headers = {
+    ...res.headers,
+    'Access-Control-Allow-Origin': getClientIp(req),
+    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept, Authorization',
+    'Access-Control-Allow-Credentials': true,
+    'Access-Control-Allow-Methods': 'OPTIONS'
+  }
+
+  let uri = URL.parse(url)
+
+  return {
+    name: name,
+    request: {
+      method: method,
+      headers: requestHeaders,
+      body: requestContent,
+      timestamp: beforeTimestamp,
+      path: uri.path,
+      querystring: uri.query
+    },
+    response: {
+      status: res.statusCode,
+      headers,
+      body: body,
+      timestamp: new Date()
+    }
+  }
+}
+
+exports.buildReturnObject = (urn, status, statusCode, headers, responseBody, orchestrations, properties, req = null) => {
+  const _headers = {
+    ...headers,
+    'Access-Control-Allow-Origin': getClientIp(req),
+    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept, Authorization',
+    'Access-Control-Allow-Credentials': true,
+    'Access-Control-Allow-Methods': 'OPTIONS'
+  }
+
+  var response = {
+    status: statusCode,
+    headers: _headers,
+    body: responseBody,
+    timestamp: new Date().getTime()
+  }
+
+  return {
+    'x-mediator-urn': urn,
+    status: status,
+    response: response,
+    orchestrations: orchestrations,
+    properties: properties
+  }
+}
